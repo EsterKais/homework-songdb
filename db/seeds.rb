c3b02f8fba6d2017d2bc59262535631a7e07a862
EsterKais <kaisester91@gmail.com>
2016-10-14 22:48:29 +0200

    Set up Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,21 @@
+# IN DEPENDENCY ORDER
+Song.delete_all
+Artist.delete_all
+
 # ADDING ARTISTS
 
-artistCris = Artist.create( {name: "Christine and the Queens"})
-artistLana = Artist.create( {name: "Lana del Rey"})
-artistOsc = Artist.create( {name: "Oscar and the Wolf"})
-artistRag = Artist.create( {name: "Rag'n'Bone"})
+artistCris = Artist.create(name: "Christine and the Queens")
+artistLana = Artist.create(name: "Lana del Rey")
+artistOsc = Artist.create(name: "Oscar and the Wolf")
+artistRag = Artist.create(name: "Rag'n'Bone")
+
+#  ADDING SONGS
+
+songCris1 = Song.create(name: "Tilted", artist: artistCris)
+songCris2 = Song.create(name: "iT", artist: artistCris)
+songCris3 = Song.create(name: "Starshipper", artist: artistCris)
+songLana1 = Song.create(name: "Ride", artist: artistLana)
+songLana2 = Song.create(name: "Video Games", artist: artistLana)
+songLana3 = Song.create(name: "Ultraviolence", artist: artistLana)
+songOsc1 = Song.create(name: "Undress", artist: artistOsc)
+songRag1 = Song.create(name: "Human", artist: artistRag)
