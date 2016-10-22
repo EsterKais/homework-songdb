1ada83610a3f810f44a723b3cb89e34d8cd1974c
EsterKais <kaisester91@gmail.com>
2016-10-22 14:43:59 +0200

    Added Filtering Tests

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe Artist, type: :model do
       expect(artist.errors).to have_key(:name)
     end
   end
+
+  describe "#order_by_name" do
+    subject { Artist.order_by_name }
+    let!(:artist_z) { create :artist, name: "Zulu"}
+    let(:artist_a) { create :artist, name: "Alpha"}
+
+    it "filters by name" do
+      expect(subject).to match_array [artist_z, artist_a]
+    end
+  end
+
+  describe "#order_by_date" do
+    subject { Artist.order_by_date }
+    let!(:artist_z) { create :artist, name: "Zulu"}
+    let(:artist_a) { create :artist, name: "Alpha"}
+
+    it "filters by name" do
+      expect(subject).to match_array [artist_z, artist_a]
+    end
+  end
 end
