3387a857148422c38e42f79364a00c5bc0ca2d14
EsterKais <kaisester91@gmail.com>
2016-10-22 16:14:41 +0200

    Working Show page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,26 +1,24 @@
 class ArtistsController < ApplicationController
-# PAGES
+
   def index
     @artists = Artist.all
   end
 
+# FILTERING SYSTEM
+    def order_by_name
+      @artists = Artist.order_by_name
+      render action: :index
+    end
+
+    def order_by_date
+      @artists = Artist.order_by_date
+      render action: :index
+    end
+# END OF FILTERING SYSTEM
+
   def show
     @artist = Artist.find(params[:id])
+    # @photos = @product.photos
   end
-# END OF PAGES
-
-# FILTERING SYSTEM
-
-  def order_by_name
-    @artists = Artist.order_by_name
-    render action: :index
-  end
-
-  def order_by_date
-    @artists = Artist.order_by_date
-    render action: :index
-  end
-
-# END OF FILTERING SYSTEM
 
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,12 +2,13 @@ class Artist < ApplicationRecord
 # RELATIONSHIPS
   has_many :songs, dependent: :destroy
   has_many :photos, dependent: :destroy
+
 # VALIDATIONS
   validates :name, presence: true
 
 # FILTERING SYSTEM
 
-scope :order_by_name,        -> { order(:name) }
-scope :order_by_date,        -> { order(:created_at) }
+  scope :order_by_name,        -> { order(:name) }
+  scope :order_by_date,        -> { order(:created_at) }
 
 end
