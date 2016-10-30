d58768150c3e0613c1f569b526c6eb1565551aa7
EsterKais <kaisester91@gmail.com>
2016-10-30 07:50:44 +0100

    Add song IntTest working

--- spec/feature/manage_songs_spec.rb
+++ spec/feature/manage_songs_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
+require 'capybara'
 
 feature 'Manage songs', js: true do
   scenario 'add a new song' do
-    # Point your browser towards the home page
-    visit root_path
+    # Create an artist
+    Capybara.current_driver = Capybara.javascript_driver
+      artist = Artist.new(name: "Bla")
+      artist.save
+
+    # Point your browser towards the artist show page
+      visit "/artists/#{artist.id}"
 
     # Enter description in the text field
-    fill_in 'Enter a song..', with: 'Be Chill'
+    fill_in 'Name', with: 'Best Song Ever'
+
 
     # Press enter (to submit the form)
     page.execute_script("$('form').submit()")
+      sleep(2)
 
     # Expect the new song to be displayed in the list of songs
-    expect(page).to have_content('Be Chill')
+    expect(page).to have_content('Best Song Ever')
   end
 end
